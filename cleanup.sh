6737a088b34c0472146f8b5c20cfbc182ace9db5
Andi Hermanto <ahermanto47@gmail.com>
2021-12-23 16:23:55 -0500

    fix cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-oc delete route sb-camel-rest-postgresql
+oc delete deploymentconfig sb-camel-rest-postgresql
 
-oc delete service sb-camel-rest-postgresql
+oc delete buildconfig sb-camel-rest-postgresql
 
 oc delete imagestream sb-camel-rest-postgresql
 
-oc delete buildconfig sb-camel-rest-postgresql
+oc delete service sb-camel-rest-postgresql
+
+oc delete route sb-camel-rest-postgresql
+
+
+
 
